b26dfe4267e565fc7ac63f4699a42822541e7d88
Jacob DeBenedetto <deben3@gmail.com>
2024-03-06 22:07:53 -0700

    Update demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import './App.css'
 import { Puck } from "@measured/puck";
 import "@measured/puck/puck.css";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 // Create Puck component config
 const config = {
   components: {
     HeadingBlock: {
       fields: {
-        children: {
+        title: {
+          type: "text",
+        },
+        resolvedTitle: {
           type: "text",
         },
       },
-      render: ({ children }) => {
-        return <h1>{children}</h1>;
+      resolveData: async ({ props }) => {
+        return {
+          props: {
+            resolvedTitle: props.title,
+          },
+        };
+      },
+      render: ({ resolvedTitle }) => {
+        return <h1>{resolvedTitle}</h1>;
+      },
+    },
+    WYSIWYG: {
+      fields: {
+        title: {
+          type: "custom",
+          render: ({ name, onChange, value }) => ( // TODO: Connect fields to data
+            <Editor
+              toolbarClassName="toolbarClassName"
+              wrapperClassName="wrapperClassName"
+              editorClassName="editorClassName"
+              onEditorStateChange={(e) => onChange(e.currentTarget.value)}
+            />
+          ),
+        },
+      },
+      render: ({ title }) => {
+        return <p>{title}</p>;
       },
     },
   },
